46f9a75a550a3a12c8c46d2763679bf45b685d1c
Willian Agostini <willian.agostini94@gmail.com>
2017-07-07 11:37:49 -0300

    criado botao voltar para pagina inicial

--- js/app/views/repository/repositorydetailsview.js
+++ js/app/views/repository/repositorydetailsview.js
@@ -6,7 +6,11 @@ class RepositoryDetailsView {
   }
 
   template(model) {
+    localStorage.setItem('masterHtml',this.element.innerHTML);
+    console.log(this.element.innerHTML);
     this.element.innerHTML = `
+    <button id="pressBack" type="button" class="btn btn-default"> <span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span></button>
+            <br>
             <div class="panel panel-primary">
               <div class="panel-heading">
                 <h3 class="panel-title">${model.name}</h3>
@@ -18,7 +22,19 @@ class RepositoryDetailsView {
                 <a href="${model.clone_url}">${model.clone_url}</a>
               </div>
             </div>
-        `
+        `;
+
+
+    let elementName = document.getElementById('pressBack')
+    if (elementName) {
+      elementName.onclick = () => this.back();
+    }
+
+  }
+
+  back() {
+    alert('works');
+    this.element.innerHTML = localStorage.getItem('masterHtml');
   }
 
 }
