bf28b0357b84cdda920c31f81d5d68c74e461517
Willian Agostini <willian.agostini94@gmail.com>
2017-07-08 10:55:20 -0300

    organizado pastas estáticas

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-app.use('/', express.static(__dirname + '/'));
+app.use('/node_modules/', express.static(__dirname + '/node_modules/'));
+app.use('/js/', express.static(__dirname + '/js/'));
+app.use('/img/', express.static(__dirname + '/img/'));
+app.use('/css/', express.static(__dirname + '/css/'));
 
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname + '/index.html'));
-//   //__dirname : It will resolve to your project folder.
-// });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 app.listen(3000);
