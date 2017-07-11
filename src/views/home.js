5820c850bca485873b43c398c1a4c3b0ad0bbe11
Willian Agostini <willian.agostini94@gmail.com>
2017-07-11 02:19:01 -0300

    criado o layout do componente de pesquisa

--- src/views/home.js
+++ src/views/home.js
@@ -1,27 +1,55 @@
 import React, { Component } from "react";
 import {
-  Button,
   FormGroup,
-  InputGroup,
   FormControl,
-  Glyphicon,
-  DropdownButton,
-  MenuItem
+  ControlLabel,
+  HelpBlock
 } from "react-bootstrap";
 
-class Home extends Component {
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { valor: "" };
+    console.log(this);
+  }
+
+  getInitialState() {
+    return {
+      value: ""
+    };
+  }
+
+  getValidationState() {
+    let length = this.state.valor.length;
+    if (length > 10) return "success";
+    else if (length > 5) return "warning";
+    else if (length > 0) return "error";
+  }
+
+  handleChange(e) {
+    console.log(e.target);
+    // this.setState({ valor: e.target.value });
+  }
+
   render() {
     return (
-      <div>
-        <FormGroup>
-          <InputGroup>
-            <FormControl type="text" />
-            <InputGroup.Button>
-              <Button>Before</Button>
-            </InputGroup.Button>
-          </InputGroup>
+      <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.getValidationState()}
+        >
+          <ControlLabel>Working example with validation</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.valor}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+          <HelpBlock>Validation is based on string length.</HelpBlock>
         </FormGroup>
-      </div>
+      </form>
     );
   }
 }
